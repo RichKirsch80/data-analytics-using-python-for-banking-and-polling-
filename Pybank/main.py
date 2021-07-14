327ddb52a2509fd6dc94f1a2c37b3f1563fb8c10
RichKirsch80 <85321602+RichKirsch80@users.noreply.github.com>
2021-07-14 09:36:26 -0500

    final run through and completed pybank, deleted main-working as it as a temp file to test work through

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,12 +1,11 @@
 # Dependencies
 import os
 import csv
-from typing import List, Dict
 
 #Read budget_data.csv
 csvpath = os.path.join ("..","Pybank", "Resources", "budget_data.csv")
 
-
+# opens csv and defines values to be used later
 with open(csvpath, newline='') as csvfile:
     reader = csv.reader(csvfile)
     header = next(reader)
@@ -16,47 +15,57 @@ with open(csvpath, newline='') as csvfile:
     value_max = 0
     dec_max = 0
     value_min = 0
-    
+    first_month = 0
+    last_month =0
+    # For loop to count lines, add P/L column, determine greatest Increase/Decrease
     for row in reader:
-        pl_total += float(row[1])
+        pl_total = float(row[1]) + pl_total
         if line_count == 0:
             line_count += 1
+            first_month = row[1]
         else:
             line_count += 1
+            last_month = row[1] 
             if float(row[1]) > value_max:
                 inc_max, value_max = row[0], float(row[1])
             if float(row[1]) < value_min:
-                dec_max, value_min = row[0], float(row[1])   
-     
+                dec_max, value_min = row[0], float(row[1])
+    #caluculate average change over test period              
+    avg_change = (int(last_month) - int(first_month)) / (line_count - 1)
 
-
+#printing and formating of analysis
 print("Financial Analysis")
 print("----------------------")
 print("Total Months: ", line_count)
-print("Total: $", pl_total)
-print('Average Change: $', )
-print("Greatest Increase in Profits:", {inc_max}, {value_max})
-print("Greatest Decrease in Profits:", {dec_max}, {value_min})
+print("Total: $", round(pl_total))
+print('Average Change: $', round(avg_change,2))
+print("Greatest Increase in Profits:", inc_max, "($",round(value_max),")")
+print("Greatest Decrease in Profits:", dec_max, "($",round(value_min),")")
 
+ 
+#Specify the file to write to budget_analysis.txt
+output_path = os.path.join ("..","Pybank", "analysis", "budget_analysis.txt")
+
+# Open the file using "write" mode, tf variable for write function for text file with \n line break
+tf = open(output_path, 'w')
+
+# write findings into txt file and general formating 
+tf.write("Financial Analysis\n")
+tf.write("----------------------\n")
+tf.write("Total Months:")
+tf.write(" " + str(line_count) + "\n")
+tf.write("Total: $")
+tf.write(str(round(pl_total)) + "\n")
+tf.write("Average Change: $")
+tf.write(str(round(avg_change,2)) + "\n")
+tf.write("Greatest Increase in Profits:")
+tf.write(str(inc_max) + " " + "($" + str(round(value_max)) + ")" + "\n")
+tf.write("Greatest Decrease in Profits:")
+tf.write(str(dec_max) + " " + "($" + str(round(value_min)) + ")" + "\n")
 
     
+tf.close()
     
-        
-    #months = sum(1 for row in csvreader)
-    #print(months) 
-#Specify the file to write to budget_analysis.csv
-output_path = os.path.join ("..","Pybank", "analysis", "budget_analysis.csv")
-
-
-
-# Open the file using "write" mode. Specify the variable to hold the contents
-with open(output_path, 'w', newline='') as csvfile:
-
-    # Initialize csv.writer
-    csvwriter = csv.writer(csvfile, delimiter=',')
-
-    # Write (column headers)
-    csvwriter.writerow(['Total Months', 'Total Profit/Losses', 'Average Change', 'Greatest Increase', 'Greatest Decrease'])
 
   
 
